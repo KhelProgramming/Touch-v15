01929b380298dbfa717a7a62b03193eac8b1ae97
Michael Rey Belga <belgamichaelrey@gmail.com>
2026-03-08 23:47:47 +0800

    feat: add python version checker
    Add Python version check to ensure compatibility.

--- hand_controller/__main__.py
+++ hand_controller/__main__.py
@@ -1,3 +1,10 @@
+import sys
+
+# Python version check
+if sys.version_info < (3, 11) or sys.version_info >= (3, 12):
+    print("This project requires Python 3.11")
+    sys.exit(1)
+
 from .app import main
 
 if __name__ == "__main__":
